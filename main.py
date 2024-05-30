2f20dec98ad0ec6d720d22e3ceb4af3b19c339c1
caiselvass <caiselvassala@gmail.com>
2024-05-30 10:28:05 +0200

    MOD update main.py

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from src.cky import CKY
 from src.functions import split_input
 import os
 
-def main(input_text: str, file_name: str):
+def main(input_text: str, file_name: str) -> None:
+	"""
+	Function to process the input text, splitting it into the rules and words, and then parsing the words using the CKY algorithm.
+
+	Parameters
+	----------
+	input_text (str): The input text of the file to be processed.
+	file_name (str): The name of the file being processed.
+	"""
 	rules_text, words = split_input(file_text=input_text)
 	cfg = CFG(from_text=rules_text)
 
@@ -16,7 +24,7 @@ def main(input_text: str, file_name: str):
 		results.append(result)
 
 	# Save the results to a file
-	save_path = os.path.join('./results', f"results_{file_name}")
+	save_path = f"./results/results_{file_name}"
 
 	save_file = open(save_path, 'w', encoding='utf-8')
 
@@ -45,24 +53,29 @@ def main(input_text: str, file_name: str):
 	print(f"Results saved to '{save_path}'.")
 
 if __name__ == '__main__':
+	# Ask the user for the file to process
 	all_files = False
 	first_n_files = None
-	# Read the input from the stdin
 	while True:
-		try:
-			file_name = input("File: ")
-			if not file_name:
-				raise ValueError("Please provide a valid file name.")
-			
-			elif file_name.lower() == 'all':
-				all_files = True
-			
-			elif not os.path.isfile(os.path.join('./tests', file_name)):
-				raise FileNotFoundError(f"File '{file_name}' not found. Please provide a valid file name in the folder './tests'.")
-			
-			break
-		except ValueError as e:
-			print(e)
+		file_name = input("Input file: ")
+		if not file_name:
+			print("Please provide a valid file name.")
+			continue
+
+		if file_name.lower() == 'help':
+			print("You can input the following commands:")
+			print("\t- A file name to process a specific file in the folder './tests'.")
+			print("\t- 'all' to process all files in the folder './tests'.")
+			continue
+		
+		elif file_name.lower() == 'all':
+			all_files = True
+		
+		elif not os.path.isfile(os.path.join('./tests', file_name)):
+			print(f"File '{file_name}' not found. Please provide a valid file name in the folder './tests'.")
+			continue
+		
+		break
 
 	# Get the list of files to process
 	files_list = os.listdir('./tests') if all_files else [file_name]
@@ -70,7 +83,8 @@ if __name__ == '__main__':
 	for file_name in files_list:
 		with open(os.path.join('./tests', file_name), 'r', encoding='utf-8') as file:
 			input_text = file.read()
-			main(input_text=input_text, file_name=file_name)
-			print(f"Processed file '{file_name}' successfully.")
 
-	print("All files processed successfully!")
+			# Process the file
+			main(input_text=input_text, file_name=file_name)
+
+	print("\nAll files processed successfully!")
